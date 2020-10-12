ad7a2412072ab27f8b86b1c75f25ddf842a9482a
Einar Persson <me@einarpersson.com>
2020-10-12 14:18:38 +0200

    3 and 5

---
+++ A-Beginner/3/script.js
@@ -0,0 +1,5 @@
+// Det här är en tom array.
+const names = []
+
+// Printa ut den tomma arrayen
+console.log(names)
---
+++ A-Beginner/5/script.js
@@ -0,0 +1,12 @@
+// Deklarera en variabel
+// Kan göras på flera sätt
+
+// Gammalt sätt
+var x = 5
+
+// konstant variabel
+const y = 7
+
+// icke-konstant variabel
+let z = 4
+
